fca81cdf89b6e0f23c584459014c37d6ee5e2948
Hooria Ahmed <72269201+hooria178@users.noreply.github.com>
2024-08-16 07:17:03 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Marcello AI",
-  description: "Get Quick Recipies with the ingredients you input!",
+  description: "Get quick recipies with the ingredients you input!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-28YLLZFDSG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-28YLLZFDSG');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
